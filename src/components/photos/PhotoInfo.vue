454061066fe54e69c62d9bfa0015156570290073
vsdeveloper <liulongbin1314@outlook.com>
2018-09-22 22:54:08 +0800

    修复了vue-preview插件报错，以及修改了请求根路径为http://www.liulongbin.top:3005

--- src/components/photos/PhotoInfo.vue
+++ src/components/photos/PhotoInfo.vue
@@ -10,7 +10,9 @@
 
     <!-- 缩略图区域 -->
     <div class="thumbs">
-      <img class="preview-img" v-for="(item, index) in list" :src="item.src" height="100" @click="$preview.open(index, list)" :key="item.src">
+      <!-- 注意：这里使用的 vue-preview 组件是版本 1.1.3,
+           之前上课录屏时候, 所使用的 1.0.5 版本的 vue-preview 存在 Bug -->
+      <vue-preview :slides="list"></vue-preview>
     </div>
 
     <!-- 图片内容区域 -->
@@ -23,7 +25,7 @@
 
 <script>
 // 1. 导入评论子组件
-import comment from "../subcomponents/comment.vue";
+import comment from '../subcomponents/comment.vue'
 
 export default {
   data() {
@@ -31,41 +33,43 @@ export default {
       id: this.$route.params.id, // 从路由中获取到的 图片Id
       photoinfo: {}, // 图片详情
       list: [] // 缩略图的数组
-    };
+    }
   },
   created() {
-    this.getPhotoInfo();
-    this.getThumbs();
+    this.getPhotoInfo()
+    this.getThumbs()
   },
   methods: {
     getPhotoInfo() {
       // 获取图片的详情
-      this.$http.get("api/getimageInfo/" + this.id).then(result => {
+      this.$http.get('api/getimageInfo/' + this.id).then(result => {
         if (result.body.status === 0) {
-          this.photoinfo = result.body.message[0];
+          this.photoinfo = result.body.message[0]
         }
-      });
+      })
     },
     getThumbs() {
       // 获取缩略图
-      this.$http.get("api/getthumimages/" + this.id).then(result => {
+      this.$http.get('api/getthumimages/' + this.id).then(result => {
         if (result.body.status === 0) {
           // 循环每个图片数据，补全图片的宽和高
           result.body.message.forEach(item => {
-            item.w = 600;
-            item.h = 400;
-          });
+            item.w = 600
+            item.h = 600
+            item.msrc = item.src
+          })
           // 把完整的数据保存到 list 中
-          this.list = result.body.message;
+          this.list = result.body.message
+          console.log(this.list)
         }
-      });
+      })
     }
   },
   components: {
     // 注册 评论子组件
-    "cmt-box": comment
+    'cmt-box': comment
   }
-};
+}
 </script>
 
 <style lang="scss" scoped>
@@ -87,11 +91,23 @@ export default {
     font-size: 13px;
     line-height: 30px;
   }
+}
+</style>
 
-  .thumbs{
-    img{
-      margin: 10px;
-      box-shadow: 0 0 8px #999;
+<style lang="scss">
+.thumbs {
+  .my-gallery {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  figure {
+    width: 100px;
+    height: 100px;
+    margin: 10px;
+    box-shadow: 0 0 8px #999;
+    img {
+      width: 100%;
+      height: 100%;
     }
   }
 }
--- src/components/subcomponents/swiper.vue
+++ src/components/subcomponents/swiper.vue
@@ -4,7 +4,7 @@
       <!-- 在组件中，使用v-for循环的话，一定要使用 key -->
       <!-- 将来，谁使用此 轮播图组件，谁为我们传递 lunbotuList -->
       <!-- 此时，lunbotuList 应该是 父组件向子组件传值来设置 -->
-      <mt-swipe-item v-for="item in lunbotuList" :key="item.url">
+      <mt-swipe-item v-for="item in lunbotuList" :key="item.id">
         <img :src="item.img" alt="" :class="{'full': isfull}">
       </mt-swipe-item>
     </mt-swipe>
